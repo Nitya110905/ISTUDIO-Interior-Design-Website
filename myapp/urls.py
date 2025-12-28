15a322c6b5ab8d321ecd6aae0c3187e36609e0a4
Nitya Shah <ncs110905@gmail.com>
2025-12-28 16:19:10 +0530

    About page

--- myapp/urls.py
+++ myapp/urls.py
@@ -19,6 +19,7 @@ from django.urls import path,include
 from myapp import views
 
 urlpatterns = [
-    path('index/', views.index, name='index')
+    path('index/', views.index, name='index'),
+    path('about', views.about, name = 'about'),
 ]
 
